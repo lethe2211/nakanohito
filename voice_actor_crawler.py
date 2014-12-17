1cfcc86d09fc2f734c112da7a115ea55c374bbdb
lethe <lethe2211@gmail.com>
2014-12-17 23:24:15 +0900

    use numpy

--- voice_actor_crawler.py
+++ voice_actor_crawler.py
@@ -1,12 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import time
 import csv
-import itertools
-from multiprocessing import Process
+import numpy as np
+# import itertools
+# from multiprocessing import Process
 from voice_actor_api_wrapper import VoiceActorApiWrapper
 from relevance_between_voice_actors import RelevanceBetweenVoiceActors
 
@@ -17,75 +15,54 @@ class VoiceActorCrawler(object):
         self.rbva = RelevanceBetweenVoiceActors()
 
     def crawl(self):
-        vids = self.vaaw.get_id_list()['data']['vid_list']
+        vids = set(self.vaaw.get_id_list()['data']['vid_list'])
+
+        a = np.array([[-1.0 for j in xrange(4200)] for i in xrange(4200)])
+
+        for i in xrange(4200):
+            for j in xrange(4200):
+                if i in vids and j in vids:
+                    a[i][j] = self.get_relevance(i, j)
+
+        np.savez('jaccard')
+
+        # vids = self.vaaw.get_id_list()['data']['vid_list']
         
-        min_relevance = float('inf')
-        result = {}
+        # min_relevance = float('inf')
+        # result = {}
 
-        for vid1, vid2 in list(itertools.combinations(vids, 2)):
-            row = self.get_relevance(vid1, vid2)
-            if len(result) < 100:
-                result[(vid1, vid2)] = row
-                if min_relevance > row[2]:
-                    min_relevance = row[2]
-                # min_relevance = min(e[2] for e in result.values())
-            else:
-                if min_relevance < row[2]:
-                    result[(vid1, vid2)] = row
-                    for (v1, v2), (name1, name2, relevance) in result.items():
-                        if relevance == min_relevance:
-                            print 'Poped', result[(v1, v2)][0], result[(v1, v2)][1], result[(v1, v2)][2]
-                            result.pop((v1, v2))
-                            break
-                    else:
-                        print 'okashii'
-                    min_relevance = min(e[2] for e in result.values())
-            if (vid1, vid2) in result:
-                print 'Added', result[(vid1, vid2)][0], result[(vid1, vid2)][1], result[(vid1, vid2)][2]
-            else:
-                print 'Not added', row[0], row[1], row[2]
-        result = sorted(result.items(), cmp=lambda x, y: cmp(y[1][2], x[1][2]))
-        print result
-        with open('relevance.csv', 'w') as f:
-            writer = csv.writer(f, lineterminator='\n')
-            for elem in result:
-                row = [elem[0][0], elem[0][1], elem[1][0].encode('utf-8'), elem[1][1].encode('utf-8'), elem[1][2]]
-                writer.writerow(row)
-
-
-
-        # vids_combination = list(itertools.combinations(vids, 2))
-        # vids_combination_divided = [vids_combination[i:i+len(vids_combination)/4] for i in range(0, len(vids_combination), len(vids_combination)/4)]
-        # jobs = [Process(target=self.get_relevances, args=(vcd,)) for vcd in vids_combination_divided]
-        # for j in jobs:
-        #     j.start()
-        # for j in jobs:
-        #     j.join()
-        # self.result = sorted(self.result.items(), cmp=lambda x, y: cmp(y[1][2], x[1][2]))
-        # return self.result
-        # for vid1, vid2 in it:
-        #     jobs = []
-        #     if len(jobs) == 5:
-        #         for j in jobs:
-        #             j.start()
-        #         jobs = []
+        # for vid1, vid2 in list(itertools.combinations(vids, 2)):
+        #     row = self.get_relevance(vid1, vid2)
+        #     if len(result) < 100:
+        #         result[(vid1, vid2)] = row
+        #         if min_relevance > row[2]:
+        #             min_relevance = row[2]
+        #         # min_relevance = min(e[2] for e in result.values())
         #     else:
-        #         jobs.append()
+        #         if min_relevance < row[2]:
+        #             result[(vid1, vid2)] = row
+        #             for (v1, v2), (name1, name2, relevance) in result.items():
+        #                 if relevance == min_relevance:
+        #                     print 'Poped', result[(v1, v2)][0], result[(v1, v2)][1], result[(v1, v2)][2]
+        #                     result.pop((v1, v2))
+        #                     break
+        #             else:
+        #                 print 'okashii'
+        #             min_relevance = min(e[2] for e in result.values())
+        #     if (vid1, vid2) in result:
+        #         print 'Added', result[(vid1, vid2)][0], result[(vid1, vid2)][1], result[(vid1, vid2)][2]
+        #     else:
+        #         print 'Not added', row[0], row[1], row[2]
+        # result = sorted(result.items(), cmp=lambda x, y: cmp(y[1][2], x[1][2]))
+        # print result
+        # with open('relevance.csv', 'w') as f:
+        #     writer = csv.writer(f, lineterminator='\n')
+        #     for elem in result:
+        #         row = [elem[0][0], elem[0][1], elem[1][0].encode('utf-8'), elem[1][1].encode('utf-8'), elem[1][2]]
+        #         writer.writerow(row)
 
 
-        # jobs = [Process(target=self.get_relevance, args=(vid1, vid2,)) for vid1, vid2 in itertools.combinations(vids, 2)]
-        # print jobs
-        # for j in jobs:
-        #     j.start()
-        # for j in jobs:
-        #     j.join()
-        #     # time.sleep(1)
-        # self.result = sorted(self.result.items(), cmp=lambda x, y: cmp(y[1][2], x[1][2]))
-        # return self.result
 
-    # def get_relevances(self, comb):
-    #     for vid1, vid2 in comb:
-    #         self.get_relevance(vid1, vid2)
 
     def get_relevance(self, vid1, vid2):
         return [self.vaaw.search(vid1)['data']['name'], self.vaaw.search(vid2)['data']['name'], self.rbva.calculate(vid1, vid2)]
